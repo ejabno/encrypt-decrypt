2fa51ab6dc2b96a7ae9d2a48141e95042c5a9375
ejabno <jbak123@live.com>
2017-10-21 13:35:01 -0600

    First time using this shit man. Fuck.

---
+++ README.txt
@@ -0,0 +1,4 @@
+Simple encryption system
+
+Also trying out git for the first time
+Well shit trying out another feature sorry dude
--- decrypt.c
+++ decrypt.c
@@ -7,7 +7,27 @@
 int main() {
     char inputInFlush[MAXINPUT]; 
     char inputToDecrypt[MAXINPUT];
+    char *decryptCursor;
+    printf("Enter encryption code: \n");
     fgets(inputInFlush, MAXINPUT, stdin);
     sscanf(inputInFlush, "%[^\n]", inputToDecrypt);
     printf("inputToDecrypt: %s\n", inputToDecrypt);
+
+    //Note to self: sizeof() != strlen()!!!!!!
+    printf("strlen() returned: %lu\n", strlen(inputToDecrypt));
+
+    int stringLength = (int) strlen(inputToDecrypt);
+    printf("stringLength = %d\n", stringLength);
+    decryptCursor = &inputToDecrypt[0];
+    for (int i = 0; i < stringLength; i++) {
+        printf("%c\n", *decryptCursor);
+        decryptCursor++;
+    }
+
+    /*decryptCursor = &inputToDecrypt[0];*/
+    /*printf("decryptCursor: %c\n", *decryptCursor);*/
+    /*decryptCursor++;*/
+    /*printf("decryptCursor: %c\n", *decryptCursor);*/
+
+    /*while ()*/
 }                                                                                                                        
